46775bf0286544e98d0c10d7e8c8dca7a6de93c7
LucasEPK <83588493+LucasEPK@users.noreply.github.com>
2025-03-24 20:10:19 -0300

    made arduino settings page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,45 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import Slider from '@mui/material/Slider';
+import { useState } from "react";
+import Paper from '@mui/material/Paper';
+import { Box, FormControlLabel, Switch } from "@mui/material";
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [valueLed9, setValueLed9] = useState<number>(30);
+  const [valueLed10, setValueLed10] = useState<number>(30);
+  const [valueLed11, setValueLed11] = useState<number>(30);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const handleChange9 = (event: Event, newValue: number | number[]) => {
+    setValueLed9(newValue as number);
+  };
+
+  const handleChange10 = (event: Event, newValue: number | number[]) => {
+    setValueLed10(newValue as number);
+  };
+
+  const handleChange11 = (event: Event, newValue: number | number[]) => {
+    setValueLed11(newValue as number);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Paper elevation={3} className={styles.paper}>
+        <div className={styles.innerContainer}>
+          <h1>Arduino settings</h1>
+          <p>Led 9 intensity</p>
+          <Slider aria-label="Led 9 intensity" value={valueLed9} onChange={handleChange9}/>
+          <p>Led 10 intensity</p>
+          <Slider aria-label="Led 10 intensity" value={valueLed10} onChange={handleChange10}/>
+          <p>Led 11 intensity</p>
+          <Slider aria-label="Led 11 intensity" value={valueLed11} onChange={handleChange11}/>
+          <FormControlLabel control={<Switch />} label="Led 13" />
+          <p>LDR intensity: 0</p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        
+      </Paper>
     </div>
   );
 }
