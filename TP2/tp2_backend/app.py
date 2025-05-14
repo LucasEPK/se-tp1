2f36696d7eba63c10f7039509324771efe88c9da
Lucas <lucasmoyano11@outlook.it>
2025-05-14 10:28:44 -0300

    TP2: signal is now sent to arduino to turn on/off analog read

--- TP2/tp2_backend/app.py
+++ TP2/tp2_backend/app.py
@@ -30,14 +30,22 @@ def read_serial():
                 
                 if line:
                     if line.startswith("luminosity:"):
+                        # Sends luminosity value to frontend
                         # luminosity: 0000
                         # Extract the luminosity value from the line
                         ldrLuminosity = int(line[12:16])
                         print("LDR luminosity: ", ldrLuminosity)
                         
                         socketio.emit("update_arduino_data", {"ldr_luminosity": ldrLuminosity})
-                    #else:
-                    #    print("not a case: ", line)
+                    elif line.startswith("isReadOn:"):
+                        # Sends analog read on/off value to frontend
+                        # isReadOn: 0
+                        analogReadOn = bool(int(line[10:11]))
+
+                        print("Analog read on: ", analogReadOn)
+                        socketio.emit("update_analog_read_on", {"analog_read_on": analogReadOn})
+                    else:
+                        print("not a case: ", line)
             except Exception as e:
                 print(f"Error reading serial data: {e}")
         time.sleep(1.0)
@@ -47,6 +55,19 @@ threading.Thread(target=read_serial, daemon=True).start()
 @socketio.on('connect')
 def handle_connect():
     print("Client connected")
+
+# Sends on/off signal to analog read task in arduino
+@app.post('/api/switch_analog_read')
+def switch_analog_read():
+    data = request.get_json()
+
+    analogReadOn = data["analogReadOn"]
+    # Send the command to the Arduino
+    cadena = 't' if analogReadOn == True else 'f'
+    print(cadena)
+    ser.write(cadena.encode('utf-8'))
+    return jsonify({"status": "success", "message": "Analog read updated"}), 200
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, port=8080, host="192.168.100.99")
--- TP2/tp2_frontend/app/page.tsx
+++ TP2/tp2_frontend/app/page.tsx
@@ -11,18 +11,51 @@ const backendUrl = "http://192.168.100.99:8080";
 const socket = io(backendUrl);
 
 export default function Home() {
-  const [alarmOn, setAlarmOn] = useState<boolean>(true);
+  const [alarmOn, setAlarmOn] = useState<boolean>(false);
   const [luminosity, setLuminosity] = useState<number>(0);
+  const [analogReadOn, setAnalogReadOn] = useState<boolean>(true);
 
   useEffect(() => {
+    // receives luminosity data from the backend and sets alarm on if > 800
     socket.on("update_arduino_data", (data) => {
       setLuminosity(data.ldr_luminosity);
+
+      if (data.ldr_luminosity > 800) {
+        setAlarmOn(true);
+      }
     });
 
-  }, [socket, luminosity]);
+    // receives analog read on/off data from the backend and sets alarm off if analog read is off
+    socket.on("update_analog_read_on", (data) => {
+      if (analogReadOn != data.analog_read_on) {
+        setAnalogReadOn(data.analog_read_on);
+      }
 
-  const switchLed = () => {
-    console.log("Switch toggled");
+      if (! data.analog_read_on) {
+        setAlarmOn(false);
+      }
+    });
+  }, [socket, luminosity, analogReadOn]);
+
+  // Sends post request to the backend to switch the analog read on or off and 
+  // turns alarm off if analog read is off
+  async function switchRead() {
+    setAnalogReadOn(!analogReadOn);
+
+    if (analogReadOn == true) {
+      setAlarmOn(false);
+    }
+
+    const response = await fetch(`${backendUrl}/api/switch_analog_read`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ analogReadOn: !analogReadOn }),
+    });
+    const data = await response.json();
+
+  
   }
 
   return (
@@ -33,7 +66,7 @@ export default function Home() {
           <h1>TP2: freeRTOS</h1>
           <p>Luminosity: {luminosity}</p>
           <p>Analog Read:</p>
-          <Switch defaultChecked onChange={switchLed}/>
+          <Switch checked={analogReadOn} onChange={switchRead}/>
           {alarmOn ? <Alert severity="warning">LUMINOSITY REACHED 800+ VALUE</Alert> : <p>Lucas Moyano</p>}
           
         </div>
