5b476b5d1ff5444ed29e58b333e38ad1e4b14b38
Lucas <lucasmoyano11@outlook.it>
2025-05-19 17:36:21 -0300

    TP3: added api connection

--- TP3/backend/app.py
+++ TP3/backend/app.py
@@ -27,12 +27,20 @@ response = cliente.request('pool.ntp.org', version=3)
 print("Hora unix: ", str(round(response.tx_time)))
 print("Hora traducida: ", ctime(response.tx_time))
 
-@app.post('/api/pass_time')
-def pass_time():
+@app.post('/api/update_time')
+def update_time():
     response = cliente.request('pool.ntp.org', version=3)
     time = str(round(response.tx_time))
     ser.write(time.encode('utf-8'))
     return {"time": time}
+
+@app.get('/api/load_data')
+def load_data():
+    return {"data": "data"}
+
+@app.post('api/delete_data')
+def delete_data():
+    return {"data": "data"}
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, port=8080, host="localhost")
--- TP3/frontend/app/page.tsx
+++ TP3/frontend/app/page.tsx
@@ -5,19 +5,44 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { Paper, Button } from "@mui/material";
 
+const backendUrl = "http://192.168.100.99:8080";
+
 export default function Home() {
 
   function update_time() {
     console.log("actualizando hora");
-  
+    
+    fetch(backendUrl + "/api/update_time", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
   }
 
   function load_data() {
     console.log("cargando datos");
+
+    fetch(backendUrl + "/api/load_data", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
   }
 
   function delete_data() {
     console.log("borrando datos");
+
+    fetch(backendUrl + "/api/delete_data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
   }
 
   return (
