f6ccedee33c250f268f59667921d50c809f221c7
Lucas <lucasmoyano11@outlook.it>
2025-05-25 23:31:51 -0300

    TP3: finished but weird bug w cargar datos

--- TP3/backend/app.py
+++ TP3/backend/app.py
@@ -1,5 +1,6 @@
 import ntplib
-from time import ctime
+from time import ctime, sleep
+import threading
 
 from flask import Flask
 from flask_cors import CORS
@@ -10,37 +11,76 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
-#ser = serial.Serial(port='/dev/ttyACM0',
-#                    baudrate=9600,
-#                    bytesize=serial.EIGHTBITS,
-#                    parity=serial.PARITY_NONE,
-#                    stopbits=serial.STOPBITS_ONE,
-#                    timeout=1,
-#                    xonxoff=False,
-#                    rtscts=False,
-#                    dsrdtr=False,
-#                    inter_byte_timeout=None,
-#                    exclusive=None)
+ser = serial.Serial(port='/dev/ttyACM0',
+                    baudrate=9600,
+                    bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    timeout=1,
+                    xonxoff=False,
+                    rtscts=False,
+                    dsrdtr=False,
+                    inter_byte_timeout=None,
+                    exclusive=None)
 
 cliente = ntplib.NTPClient() 
 response = cliente.request('pool.ntp.org', version=3)
 print("Hora unix: ", str(round(response.tx_time)))
 print("Hora traducida: ", ctime(response.tx_time))
 
+# Reads arduino serial port output, and sends it to frontend if string matches case
+def read_serial():
+    while True:
+        if ser.in_waiting > 0:
+            try:
+                line = ser.readline().decode('utf-8').rstrip()
+                
+                if line:
+                    if line.startswith("segundos: "):
+                        actual_time = ctime(int(line[10:]))
+                        print("Hora actual: ", actual_time)
+                    if line.startswith("_eeprom"):
+                        #_eeprom 10:d 10:d 23:o 23:o 0:2 0:2 0:2 0:3 0:3 4:3 9:2 9:2 9:2
+                        events = line.split(" ")[1:] # [1:] to skip the "_eeprom" part
+                        events_string = ""
+                        for event in events:
+                            
+                            secs, event_type = event.split(":")
+                            secs = ctime(int(secs))
+                            if event_type == '2':
+                                event_type = 'Apretado botón 2'
+                            elif event_type == '3':
+                                event_type = 'Apretado botón 3'
+                            
+                            events_string += f"{secs} - {event_type}\n"
+                            print(f"Time: {secs}, Event Type: {event_type}")
+                        socketio.emit('read_events', {'events': events_string})
+                    else:
+                        print("not a case: ", line)
+                
+            except Exception as e:
+                print(f"Error reading serial data: {e}")
+        sleep(1.0)
+
+threading.Thread(target=read_serial, daemon=True).start()
+
 @app.post('/api/update_time')
 def update_time():
     response = cliente.request('pool.ntp.org', version=3)
-    time = str(round(response.tx_time))
+    time = 'time:' + str(round(response.tx_time))
     ser.write(time.encode('utf-8'))
     return {"time": time}
 
-@app.get('/api/load_data')
+@app.post('/api/load_data')
 def load_data():
+    ser.write('load'.encode('utf-8'))
     return {"data": "data"}
 
-@app.post('api/delete_data')
+@app.post('/api/delete_data')
 def delete_data():
+    cadena = 'delete'
+    ser.write(cadena.encode('utf-8'))
     return {"data": "data"}
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=8080, host="localhost")
+    socketio.run(app, debug=True, port=8080, host="192.168.100.99")
--- TP3/frontend/app/page.tsx
+++ TP3/frontend/app/page.tsx
@@ -1,13 +1,24 @@
 'use client';
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Paper, Button } from "@mui/material";
+import io from "socket.io-client";
 
 const backendUrl = "http://192.168.100.99:8080";
 
+const socket = io(backendUrl);
+
 export default function Home() {
+  const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    socket.on("read_events", (data) => {
+      setText(String(data.events));
+      console.log("read data: " + String(data.events));
+    });
+  }, [socket, text]);
 
   function update_time() {
     console.log("actualizando hora");
@@ -22,10 +33,11 @@ export default function Home() {
   }
 
   function load_data() {
+    setText("");
     console.log("cargando datos");
 
     fetch(backendUrl + "/api/load_data", {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
@@ -52,8 +64,11 @@ export default function Home() {
         <div className={styles.innerContainer}>
 
           <h1>TP3: memoria EEPROM</h1>
-          <Button onClick={update_time} variant="outlined">Actualizar hora desde el servidor NTP</Button>
+          <Button onClick={update_time} variant="outlined">Actualizar hora desde servidor NTP</Button>
           <Button onClick={load_data} variant="outlined">Cargar datos desde EEPROM</Button>
+          {text.split("\n").map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
           <Button onClick={delete_data} variant="outlined">Borrar datos de EEPROM</Button>
 
         </div>
