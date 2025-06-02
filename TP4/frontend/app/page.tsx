d8866ad8b071cc86c16cd453d83778cbd7d69e72
Lucas <lucasmoyano11@outlook.it>
2025-06-01 21:04:35 -0300

    TP4: did graphic part of frontend

--- TP4/frontend/app/page.tsx
+++ TP4/frontend/app/page.tsx
@@ -3,17 +3,52 @@ import React from "react";
 import { useState, useEffect} from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Paper, Button } from "@mui/material";
+import { Paper, Button, Alert } from "@mui/material";
 import io from "socket.io-client";
+import SaveIcon from "@mui/icons-material/Save";
+
 
 const backendUrl = "http://192.168.100.99:8080";
 
 const socket = io(backendUrl);
 
+let luxThreshold = 10000; // Lux threshold for plant health
+
 export default function Home() {
+  const [luxPerMinute, setLuxPerMinute] = useState<number>(0);
+  const [firstButton, setFirstButton] = useState(
+    <Button onClick={getLuxPerMinute} variant="outlined">Obtener luminosidad por minuto</Button>
+  );
+  const [response, setResponse] = useState(<> </>);
+
+  function getLuxPerMinute() {
+    console.log("Obteniendo luminosidad por minuto");
+    setLuxPerMinute(20000); // Simulated value, replace with actual logic
+    setFirstButton(
+      <Button fullWidth loading loadingPosition="start" startIcon={<SaveIcon/>} variant="outlined">
+        Obteniendo luminosidad, espere un minuto...
+      </Button>
+    );
+    setResponse(
+      luxPerMinute >= luxThreshold ? (
+        <Alert severity="success">La planta tiene la suficiente luminosidad</Alert>
+      ) : (
+        <Alert severity="warning">La planta no tiene la suficiente luminosidad</Alert>
+      )
+    );
+  }
+
   return (
-    <div className={styles.page}>
-    
+    <div className={styles.container}>
+      <Paper elevation={3} className={styles.paper}>
+
+        <div className={styles.innerContainer}>
+
+          <h1>TP integrador: luminosidad para plantas de vid</h1>
+          {firstButton}
+          {response}
+        </div>
+      </Paper>
     </div>
   );
 }
